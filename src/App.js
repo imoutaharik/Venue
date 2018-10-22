1d6443516c32d37182a6347502c882a5fa6eabcd
Ilham Moutaharik <ilham@prescrypto.com>
2018-10-22 18:59:58 -0500

    Create react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state={
+    coordinates:''
+  }
+
+  componentDidMount(){
+    this.getLocation()
+  }
+
+  getLocation = () =>{
+    navigator.geolocation.getCurrentPosition(response =>{
+      this.setState({
+        coordinates: response.coords.latitude+','+response.coords.longitude
+      })
+      console.log(this.state)
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
       </div>
     );
   }
