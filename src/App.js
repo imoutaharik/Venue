5fc1f1686ffa5154f195779e166a80ff61906c93
Ilham Moutaharik <ilham@prescrypto.com>
2018-10-22 19:44:30 -0500

    Functionality : get info from foursquare api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
 class App extends Component {
   state={
-    coordinates:''
+    coordinates:'',
+    venues:[]
   }
 
   componentDidMount(){
@@ -15,14 +16,34 @@ class App extends Component {
     navigator.geolocation.getCurrentPosition(response =>{
       this.setState({
         coordinates: response.coords.latitude+','+response.coords.longitude
+      }, ()=>{
+        this.getVenues('sushi')
       })
+    })
+  }
+
+  getVenues = (query) =>{
+    const url = 'https://api.foursquare.com/v2/venues/explore?';
+    const params = {
+      client_id:'MANRL0YHN23ZJOZTG4XRCMSM3NDYRP4VLX5SAN1T3YBQBXIZ',
+      client_secret:'ZXKSYHN5UBUH0AUY0CY1QJT3BVTYW2SGHSSV5B4UARX52ZYA',
+      ll:this.state.coordinates,
+      query:query,
+      v:"20181022"
+    }
+    axios.get(url + new URLSearchParams(params))
+    .then(res =>{
+      this.setState({venues:res.data.response.groups[0].items})
       console.log(this.state)
     })
   }
 
   render() {
     return (
-      <div className="App">
+      <div>
+        <ul>
+        {this.state.venues.map((v,i)=> <li key={i}>{v.venue.name}</li>)}
+        </ul>
       </div>
     );
   }
